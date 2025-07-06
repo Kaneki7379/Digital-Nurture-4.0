87c69afbd72d966eae8e37547622f6adec3d14be
kaneki7379 <singh.yash9919@gmail.com>
2025-07-07 00:50:29 +0530

    completed the exercises

---
+++ Week 3 Hands on/Spring_Core_Maven/Code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,23 @@
+package com.library.service;
+
+import com.library.model.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
---
+++ Week 3 Hands on/Spring_Core_Maven/Code/LibraryManagement/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main method for custom logic, not used by Spring Boot.");
+    }
+}
